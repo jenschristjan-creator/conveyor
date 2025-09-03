9f0f49f183c5935e87d95683ff2f9e447d6ad7a4
jenschristjan-creator <jens.christjan@gmail.com>
2025-09-03 16:23:00 +0200

    lavet et belte som kan kører eller lade vær ud fra den givne vægt

--- main.c
+++ main.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+int antalmotor;
+int vaegtpakke;
+bool beltekorer;
+float kgmotor = 5.6;
+
+void submitinfo(int m, int p){
+    antalmotor = m;
+    vaegtpakke = p;
+    float kapasitet = kgmotor * antalmotor;
+
+    if(p<=kapasitet) {
+        printf("beltet kan håndtere vægten");
+    } else {
+        printf("beltet kan ikke håndtere vægten");
+    }
+    return;
+}
+int main(void){
+    submitinfo(5,100);
+    return 0;
+}
